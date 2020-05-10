40c9f197e1cbfeb74d1a8c3d2f86c25356be739a
pierre <xpierro@gmail.com>
2020-05-10 15:58:22 +0800

    Last bounding box issue resolved, now as fast as offcial with no artifact
    TODO: anti-aliasing

--- app/src/main/java/com/sony/dpt/override/ViewOverride.java
+++ app/src/main/java/com/sony/dpt/override/ViewOverride.java
@@ -9,6 +9,7 @@ import java.lang.reflect.Method;
 public class ViewOverride implements IViewOverride {
 
     private static Method invalidateRect;
+    private static Method invalidate;
     private static ViewOverride instance;
 
     private boolean loaded;
@@ -16,6 +17,7 @@ public class ViewOverride implements IViewOverride {
     private ViewOverride() {
         try {
             invalidateRect = View.class.getMethod("invalidate", Rect.class, int.class);
+            invalidate = View.class.getMethod("invalidate", int.class);
             invalidateRect.setAccessible(true); // Small acceleration
             loaded = true;
         } catch (Exception ignored) {
@@ -40,6 +42,15 @@ public class ViewOverride implements IViewOverride {
         Rect temp = new Rect();
         rect.roundOut(temp);
         invalidate(view, temp, updateMode);
+    }
+
+    @Override
+    public void invalidate(View view, int updateMode) {
+        try {
+            invalidate.invoke(view, updateMode);
+        } catch (Exception ignored) {
+            view.invalidate();
+        }
     }
 
     @Override
